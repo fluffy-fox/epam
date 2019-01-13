a4223313274048ef88b58e908733f4470053ecd7
igor <igorsilinsky88@gmail.com>
2019-01-13 20:34:29 +0300

    req from user

--- pythonFinalTask/tag_count.py
+++ pythonFinalTask/tag_count.py
@@ -4,19 +4,23 @@ import time
 import boto3
 
 
-url = "https://google.com"
-page = requests.get(url)
-x = page.content
-soup = BeautifulSoup(x, "html.parser")
-y = [tag.name for tag in soup.find_all()]
-result = dict((i, y.count(i)) for i in y)
+def tag_counter(url: str="https://google.com", log_file: str="temp.log", s3_bucket: str='epamcourse'):
+    page = requests.get(url)
+    x = page.content
+    soup = BeautifulSoup(x, "html.parser")
+    y = [tag.name for tag in soup.find_all()]
+    result = dict((i, y.count(i)) for i in y)
+    total_count = sum(result.values())
+    date_time = time.strftime("%Y/%m/%d %H:%M")
+    log = "{}  {}  {} {} \n".format(date_time, url, total_count, result)
+    with open(log_file, 'a') as f:
+        f.write(log)
+    s3 = boto3.resource('s3')
+    s3.meta.client.upload_file(log_file, s3_bucket, log_file)
 
-total_count = sum(result.values())
-date_time = time.strftime("%Y/%m/%d %H:%M")
-log = "{}  {}  {} {} \n".format(date_time, url, total_count, result)
-with open("temp.log", 'a') as f:
-    f.write(log)
-# print(log)
+url = input('Enter site url: ')
+log_file = input('Enter log name: ')
+s3_bucket = input('Enter your s3 bucket name: ')
 
-s3 = boto3.resource('s3')
-s3.meta.client.upload_file('temp.log', 'epamcourse', 'temp.log')
+
+tag_counter(url, log_file, s3_bucket)
